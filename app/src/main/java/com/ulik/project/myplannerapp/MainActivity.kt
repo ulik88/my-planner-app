29485abe28263362a596a1e8dcd4f43a1bac9f31
Ulan Abdykerimov <ulan.abdykerimov@vmapit.de>
2021-01-20 03:44:56 +0100

    Start of Project

---
+++ app/src/main/java/com/ulik/project/myplannerapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ulik.project.myplannerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
