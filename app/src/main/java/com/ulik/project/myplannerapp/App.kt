fbc56542348c6df8be2fb26394f2f8f20249210a
Ulan Abdykerimov <ulan.abdykerimov@vmapit.de>
2021-01-20 05:13:02 +0100

    Dependencies adding

---
+++ app/src/main/java/com/ulik/project/myplannerapp/App.kt
@@ -0,0 +1,21 @@
+package com.ulik.project.myplannerapp
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class App : Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@App)
+
+            modules(
+
+            )
+        }
+    }
+}
